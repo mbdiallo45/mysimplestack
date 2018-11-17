f405a6c0a539f89c8e58009a27a794f353992c26
mbdiallo45 <dionfo45@gmail.com>
2018-11-17 22:05:14 +0100

    "first commit"

---
+++ src/main/java/deqo/bobo/mysimplestack/App.java
@@ -0,0 +1,13 @@
+package deqo.bobo.mysimplestack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/deqo/bobo/mysimplestack/AppTest.java
@@ -0,0 +1,20 @@
+package deqo.bobo.mysimplestack;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
